c550469bc12d1d8df31f57e9c30264139da7c8ad
JasonRen23 <zhicheng_ren@163.com>
2018-02-16 01:11:07 +0800

    add day1 code

--- Arrays/NextPermutation.java
+++ Arrays/NextPermutation.java
@@ -3,29 +3,19 @@ package LeetCode_idea.Arrays;
 import java.util.Arrays;
 
 public class NextPermutation {
-    private static boolean nextPermutation(int[] nums, int begin, int end) {
-        // From right to left, find the first digit(partitionNumber)
-        // which violates the increase trend
-        int p = end - 2;
-        while (p > -1 && nums[p] >= nums[p + 1]) --p;
-
-        // If not found, which means current sequence is already the largest
-        // permutation, then rearrange to the first permutation and return false
-        if(p == -1) {
-            reverse(nums, begin, end);
-            return false;
+    private static void nextPermutation(int[] nums, int begin, int end) {
+        if(nums == null || nums.length < 2) return;
+        int i = nums.length - 2;
+        while(i >= 0 && nums[i] >= nums[i + 1])
+            i--;
+        if(i >= 0){
+            int j = nums.length - 1;
+            while(j >= 0 && nums[j] <= nums[i])
+                j--;
+            swap(nums, i, j);
         }
+        reverse(nums, i + 1, nums.length - 1);
 
-        // From right to left, find the first digit which is greater
-        // than the partition number, call it changeNumber
-        int c = end - 1;
-        while (c > 0 && nums[c] <= nums[p]) --c;
-
-        // Swap the partitionNumber and changeNumber
-        swap(nums, p, c);
-        // Reverse all the digits on the right of partitionNumber
-        reverse(nums, p+1, end);
-        return true;
     }
     private static void swap(int[] nums, int i, int j) {
         int tmp = nums[i];
@@ -33,15 +23,18 @@ public class NextPermutation {
         nums[j] = tmp;
     }
     private static void reverse(int[] nums, int begin, int end) {
-        end--;
         while (begin < end) {
             swap(nums, begin++, end--);
         }
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3};
-        nextPermutation(nums,0, nums.length);
-        System.out.println("The array after permutation is " + Arrays.toString(nums));
+        int[] nums1 = {1,2,3};
+        nextPermutation(nums1,0, nums1.length);
+        System.out.println("The array after permutation is " + Arrays.toString(nums1));
+
+        int[] nums2 = {1, 5, 8, 4, 7, 6, 5, 3, 1};
+        nextPermutation(nums2,0, nums2.length);
+        System.out.println("The array after permutation is " + Arrays.toString(nums2));
     }
 }
