fd5756f830130d091f9f9f198c16896a8b44e8af
jasonren <zhicheng_ren@163.com>
2025-01-31 14:04:49 +0800

    update

---
+++ Middle_2025/Test.java
@@ -0,0 +1,12 @@
+package LeetCode_idea.Middle_2025;
+
+/**
+ * @author RenZhiCheng
+ * @since 2025/1/31 14:00
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
